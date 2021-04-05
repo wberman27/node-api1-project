86265889777ddb992dc132935674c157c853a54c
Will B <wberman27@gmail.com>
2021-04-05 17:46:01 -0400

    added get and post

--- api/server.js
+++ api/server.js
@@ -1,13 +1,37 @@
 // BUILD YOUR SERVER HERE
-const express = require('express')
-const Hero = require('./users/model.js')
+const express = require('express') //import express
+const Hero = require('./users/model.js') //allows use of model.js functions
 
-const server = express()
-server.use(express.json())
-
-
+const server = express() //server is an express app
+server.use(express.json()) //express application that responds with json
 
 
+
+//Endpoints below
+server.get('/api/users', async (req, res) =>{
+    try{
+        const getHeroes = await Hero.find()
+        res.status(200).json(getHeroes)
+    }
+    catch(err){
+        res.status(500).json({ message: "There was an error while saving the user to the database" })
+    }
+})
+
+server.post('/api/users', async (req, res) =>{
+    try{
+        const newHero = await Hero.insert(req.body)
+        
+        if(!newHero.name || !newHero.bio){
+            res.status(400).json({ message: "Please provide name and bio for the user" })
+        }else{
+            res.status(201).json(newHero)
+        }
+    }
+    catch(err){
+        res.status(500).json({ message: "There was an error while saving the user to the database" })
+    }
+})
 
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
