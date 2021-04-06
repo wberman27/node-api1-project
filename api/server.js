cfcf7630cfb37514511104ba7b6adf0f95f93db4
Will B <wberman27@gmail.com>
2021-04-05 20:32:03 -0400

    added the rest of the endpoints, 15/16 passing tests

--- api/server.js
+++ api/server.js
@@ -14,14 +14,29 @@ server.get('/api/users', async (req, res) =>{
         res.status(200).json(getHeroes)
     }
     catch(err){
-        res.status(500).json({ message: "There was an error while saving the user to the database" })
+        res.status(500).json({ message: "The users information could not be retrieved" })
+    }
+})
+
+server.get('/api/users/:id', async (req, res) =>{
+    try{
+        const {id} = req.params;
+        const getHero = await Hero.findById(id)
+        if(!getHero){
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        }else{
+            res.status(201).json(getHero)
+        }
+    }
+    catch(err){
+        res.status(500).json({ message: "The user information could not be retrieved" })
     }
 })
 
 server.post('/api/users', async (req, res) =>{
     try{
         const newHero = await Hero.insert(req.body)
-        
+
         if(!newHero.name || !newHero.bio){
             res.status(400).json({ message: "Please provide name and bio for the user" })
         }else{
@@ -30,6 +45,41 @@ server.post('/api/users', async (req, res) =>{
     }
     catch(err){
         res.status(500).json({ message: "There was an error while saving the user to the database" })
+    }
+})
+
+server.delete('/api/users/:id', async (req, res) =>{
+    try{
+        const {id} = req.params
+        const deleteUser = await Hero.remove(id)
+        if(!deleteUser){
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        }else{
+            res.status(201).json(deleteUser)
+        }
+    }
+    catch(err){
+        res.status(500).json({ message: "The user could not be removed" })
+    }
+})
+
+server.put('/api/users/:id', async (req, res) =>{
+    try{
+        const {id} = req.params
+        const changes = req.body
+        const updateUser = await Hero.update(id, changes)
+        if(!updateUser){
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        }else{
+            if(!updateUser.name || !updateUser.bio){
+                res.status(400).json({ message: "Please provide name and bio for the user"})
+            }else{
+                res.status(200).json(updateUser)
+            }
+        }
+    }
+    catch(err){
+        res.status(500).json({ message: "The user information could not be modified" })
     }
 })
 
